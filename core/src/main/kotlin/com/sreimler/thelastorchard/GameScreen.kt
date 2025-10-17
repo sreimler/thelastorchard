6818d3cc099229d74a9f6318e94efbda896bce01
Sören Reimler <soeren.reimler@gmail.com>
2025-10-17 16:23:25 +0200

    Load character texture and display on screen

--- core/src/main/kotlin/com/sreimler/thelastorchard/GameScreen.kt
+++ core/src/main/kotlin/com/sreimler/thelastorchard/GameScreen.kt
@@ -1,23 +1,38 @@
 package com.sreimler.thelastorchard
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.Texture.TextureFilter.Linear
+import com.badlogic.gdx.graphics.Texture.TextureFilter.Nearest
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.utils.viewport.FitViewport
 import ktx.app.KtxScreen
 import ktx.app.clearScreen
 import ktx.assets.disposeSafely
 import ktx.assets.toInternalFile
 import ktx.graphics.use
+import ktx.log.logger
 
 
-class FirstScreen : KtxScreen {
-    private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
+class GameScreen : KtxScreen {
+    private val characterTexture =
+        Texture("Character and Portrait/Character/Pre-made/Tori/idle.png".toInternalFile(), true).apply {
+            setFilter(Nearest, Nearest)
+        }
+    val characterChillFrontRegion = TextureRegion(characterTexture, 0, 0, 32, 32)
     private val batch = SpriteBatch()
 
     // Fixed timestep: 60 updates per second
     private val fixedDeltaTime = 1f / 60f
     private var accumulator = 0f
+
+    private val camera = OrthographicCamera()
+    private val fitViewport = FitViewport(640f, 360f, camera).also {
+        camera.position.set(320f, 180f, 0f)
+    }
+
+    private val log = logger<GameScreen>()
 
     override fun render(delta: Float) {
         val deltaTime = Gdx.graphics.deltaTime.coerceAtMost(0.25f)
@@ -41,14 +56,26 @@ class FirstScreen : KtxScreen {
         // Clear screen to black
         clearScreen(red = 0f, green = 0f, blue = 0f, alpha = 1f)
 
+        fitViewport.apply()
+        camera.update()
+        batch.projectionMatrix = camera.combined
+
+        val characterX = fitViewport.worldWidth / 2f
+        val characterY = fitViewport.worldHeight / 2f
+
         batch.use {
-            it.draw(image, 100f, 160f)
+            it.draw(characterChillFrontRegion, characterX, characterY)
         }
     }
 
+    override fun resize(width: Int, height: Int) {
+        super.resize(width, height)
+        fitViewport.update(width, height)
+    }
+
     override fun dispose() {
-        image.disposeSafely()
+        characterTexture.disposeSafely()
         batch.disposeSafely()
-        Gdx.app.log("GameApp", "Resources disposed")
+        log.debug { "Resources disposed" }
     }
 }
