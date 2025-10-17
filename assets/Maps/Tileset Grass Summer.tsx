ee18869827d9e54d4602d64b4a3fa190a66e5cee
Sören Reimler <soeren.reimler@gmail.com>
2025-10-17 18:32:41 +0200

    Create dummy map and render on screen with character on top

---
+++ assets/Maps/Tileset Grass Summer.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tileset Grass Summer" tilewidth="64" tileheight="64" tilecount="60" columns="6">
+ <image source="../Farm/Tileset/Modular/Tileset Grass Summer.png" width="384" height="640"/>
+ <wangsets>
+  <wangset name="Grass" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+  <wangset name="Grass" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+  <wangset name="Grass" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
+</tileset>
---
+++ assets/Maps/Water Ground animations tiles.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Water Ground animations tiles" tilewidth="64" tileheight="64" tilecount="24" columns="6">
+ <image source="../Farm/Tileset/Modular/Water Ground animations tiles.png" width="384" height="256"/>
+ <wangsets>
+  <wangset name="Water" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
+</tileset>
